e44bb4ca055673314736931172be10476c84004f
dubodel-alexey <dubodel.alexey@gmail.com>
2015-03-27 20:49:07 +0300

    Task 4 final (Fibonacci)

---
+++ Task4/ConsoleUIFibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FibonacciEnumerator;
+
+namespace ConsoleUIFibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            foreach (var number in FibonacciNumbers.GetNumbers(92))
+            {
+                i++;
+                Console.WriteLine("{0}: {1}", i, number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
